456e69e48bbadd07513274cfac150f925c653b6b
evisp <evisplaku@gmail.com>
2022-05-23 09:59:37 -0700

    Add: task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+  hash_node_t *nodes;
+  hash_node_t *new;
+  unsigned long int idx;
+
+  if (ht == NULL || key == NULL || value == NULL)
+    return 0;
+
+  idx = key_index((const unsigned char *)key, ht->size);
+  nodes = ht->array[idx];
+
+  while (nodes != NULL)
+    {
+      if (strcmp(nodes->key, key) == 0)
+	{
+	  free(nodes->value);
+	  nodes->value = strdup(value);
+	  if (nodes->value == NULL)
+	    return (0);
+	  return (1);
+	}
+      nodes = nodes->next;
+    }
+
+  new = malloc(sizeof(hash_node_t));
+  if (new == NULL)
+    return (0);
+
+  new->key = strdup(key);
+  if (new->value == NULL)
+    return (0);
+  new->next = ht->array[idx];
+  ht->array[idx] = new;
+
+  return (1);
+  
+}
