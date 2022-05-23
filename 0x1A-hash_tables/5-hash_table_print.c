cd3cd3dcd89f7744aa3c30625695c619c6023fe7
evisp <evisplaku@gmail.com>
2022-05-23 10:14:31 -0700

    Add: task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+void hash_table_print(const hash_table_t *ht)
+{
+  hash_node_t *node;
+  unsigned long int i;
+  unsigned int print_comma = 0;
+
+  if (ht == NULL)
+    return;
+
+  printf("{");
+  for (i = 0; i < ht->size; i++)
+    {
+      if (ht->array[i] != NULL)
+	{
+	  if (print_comma == 1)
+	    printf(", ");
+
+	  node = ht->array[i];
+	  while (node != NULL)
+	    {
+	      printf("'%s': '%s'", node->key, node->value);
+	      node = node->next;
+	      if (node != NULL)
+		printf(", ");
+	    }
+	  print_comma = 1;
+	}
+	
+    }
+  printf("}\n");
+}
