342843cde57af46efd11aa518339214ac7504f91
evisp <evisplaku@gmail.com>
2022-06-10 11:55:10 -0700

    add: prime factor

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	long int factor, number = 612852475143;
+
+	for (factor = 2; factor < (number / 2); factor++)
+		if (number % factor == 0)
+			number = number / factor;
+
+        printf("%ld\n", factor);
+
+        return (0);
+}
