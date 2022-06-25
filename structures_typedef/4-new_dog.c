994ebc7bd39749f8ca0548c56729308e05a66034
evisp <evisplaku@gmail.com>
2022-06-24 23:35:31 -0700

    add task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -51,7 +51,7 @@ char *_strcopy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *doggo;
+	dog_t *my_dog;
 
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
