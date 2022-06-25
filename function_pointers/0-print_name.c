d11f1eb1955845055bd0d362f4b2e1bf8b56286c
evisp <evisplaku@gmail.com>
2022-06-24 23:45:30 -0700

    add: task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name
+ * @name: the name
+ * @f: function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f && name)
+		f(name);
+}
