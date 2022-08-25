b0d752390fa1e93d5afb3c23aa26f964ae5874df
evisp <evisplaku@gmail.com>
2022-08-25 00:37:31 -0700

    add: task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,13 +3,12 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t idx;
-	char msg[] = "Value checked array";
 
 	if (array)
 	{
 		for (idx = 0; idx < size; idx++)
 		{
-			printf("%s[%ld] = [%d]\n", msg, idx, array[idx]);
+			printf("Value checked array[%ld] = [%d]\n", idx, array[idx]);
 			if (array[idx] == value)
 				return (idx);
 		}
