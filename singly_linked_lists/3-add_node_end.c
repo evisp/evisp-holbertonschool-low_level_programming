9a615474dc126343cbdb10d0cc6502e5f941c8a3
evisp <evisplaku@gmail.com>
2024-12-02 09:02:35 +0000

    add: function to add node at the end of list

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -17,15 +17,15 @@ unsigned int string_length(const char *str)
 }
 
 /**
- * add_node - Adds a new node at the beginning of a list_t list.
+ * add_node_end - Adds a new node at the end of a list_t list.
  * @head: Double pointer to the head of the list.
  * @str: String to be duplicated and added to the new node.
  *
  * Return: Address of the new element, or NULL if it failed.
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new_node, *temp;
 	char *dup_str;
 
 	if (str == NULL)
@@ -41,12 +41,21 @@ list_t *add_node(list_t **head, const char *str)
 		free(dup_str);
 		return (NULL);
 	}
-
 	new_node->str = dup_str;
 	new_node->len = string_length(dup_str);
+	new_node->next = NULL;
 
-	new_node->next = *head;
-	*head = new_node;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
 
+		temp->next = new_node;
+	}
 	return (new_node);
 }
