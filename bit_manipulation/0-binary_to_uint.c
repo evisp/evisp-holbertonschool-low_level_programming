8065dd9b9b377b3e8dec679ec32ec6380da40d7f
evisp <evisplaku@gmail.com>
2024-12-05 08:13:52 +0000

    update task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -13,10 +13,10 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-		
+
 	while (b[length] != '\0')
 		length++;
-	
+
 	for (i = (length - 1); i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
